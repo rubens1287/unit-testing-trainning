faad48e1d3fad877a1f15c8650f35bde05223620
de Medeiros Lobo <rslo@gft.com>
2023-06-14 15:50:36 -0300

    Adicionado TDD RG

--- src/test/java/RGTest.java
+++ src/test/java/RGTest.java
@@ -5,12 +5,12 @@ import org.junit.Test;
 public class RGTest {
 
     /*
-			O sistema terá como entrada um documento de RG, porem o mesmo só será aceito se passar pelos criterios de aceite
-			1. O RG não pode ser nulo
-			2. O RG não pode ser vazio
-			3. O RG deve conter o traço para separar os digitos
-			4. Validar RG válido
-		 */
+        O sistema terá como entrada um documento de RG, porem o mesmo só será aceito se passar pelos criterios de aceite
+        1. O RG não pode ser nulo
+        2. O RG não pode ser vazio
+        3. O RG deve conter o traço para separar os digitos
+        4. Validar RG válido
+     */
 
     private RG validarg;
 
@@ -22,9 +22,9 @@ public class RGTest {
     @Test
     public void testIsValidoRG(){
 
-        Assert.assertEquals("retorna False - invalido RG", false,validarg.isValidaRG("123456789"));
-        Assert.assertEquals("retorna False - invalido RG", false,validarg.isValidaRG(null));
-        Assert.assertEquals("retorna False - invalido RG",false, validarg.isValidaRG(""));
-        Assert.assertEquals("retorna True - valido RG",true ,validarg.isValidaRG("12345678-90"));
+        Assert.assertFalse("retorna False - invalido RG", validarg.isValidaRG("123456789"));
+        Assert.assertFalse("retorna False - invalido RG", validarg.isValidaRG(null));
+        Assert.assertFalse("retorna False - invalido RG", validarg.isValidaRG(""));
+        Assert.assertTrue("retorna True - valido RG", validarg.isValidaRG("12345678-90"));
     }
 }
